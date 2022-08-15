188d0c5bd396dc3c5b9e171281c6898f69f7e495
Ikiru Yoshizaki <3856350+guitarrapc@users.noreply.github.com>
2022-08-15 11:57:06 +0900

    fix: sh

--- zstd-linux.sh
+++ zstd-linux.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-docker run -it --rm -v "$PWD/builder/zstd:/builder" -v "$PWD/zstd:/src" --entrypoint /bin/sh alpine:latest -c /builder/zstd-builder.sh
+docker run --rm -v "$PWD/builder/zstd:/builder" -v "$PWD/zstd:/src" --entrypoint /bin/sh alpine:latest -c /builder/zstd-builder.sh
 ls zstd/lib/libzstd.a
 ls zstd/lib/libzstd.so*
