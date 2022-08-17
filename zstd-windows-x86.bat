7f521435d36e7ea9139635ebabe249d265fb346c
Ikiru Yoshizaki <3856350+guitarrapc@users.noreply.github.com>
2022-08-17 16:03:41 +0900

    chore: comment

--- zstd-windows-x86.bat
+++ zstd-windows-x86.bat
@@ -14,11 +14,11 @@ set PLATFORM=x64
 docker run --rm -v "%cd%/builder/zstd:/builder" -v "%cd%/zstd:/src" ubuntu:22.04 /bin/bash /builder/zstd-builder-windows-x86.sh
 ::docker run --rm -v "%cd%/builder/zstd:/builder" -v "%cd%/zstd:/src" guitarrapc/ubuntu-mingw-w64:22.04.1 /bin/bash /builder/zstd-builder-windows-x86.sh
 
-@REM :: confirm
-@REM dir zstd\lib\dll\libzstd.dll
-@REM dir zstd\programs\zstd.exe
+:: confirm
+dir zstd\lib\dll\libzstd.dll
+dir zstd\programs\zstd.exe
 
-@REM :: copy
-@REM mkdir pkg\zstd\%GIT_ZSTD_VERSION%\%OS%\%PLATFORM%
-@REM cp .\zstd\lib\dll\libzstd.dll .\pkg\zstd\%GIT_ZSTD_VERSION%\%OS%\%PLATFORM%\libzstd.dll
-@REM cp .\zstd\programs\zstd.exe .\pkg\zstd\%GIT_ZSTD_VERSION%\%OS%\%PLATFORM%\zstd.exe
+:: copy
+mkdir pkg\zstd\%GIT_ZSTD_VERSION%\%OS%\%PLATFORM%
+cp .\zstd\lib\dll\libzstd.dll .\pkg\zstd\%GIT_ZSTD_VERSION%\%OS%\%PLATFORM%\libzstd.dll
+cp .\zstd\programs\zstd.exe .\pkg\zstd\%GIT_ZSTD_VERSION%\%OS%\%PLATFORM%\zstd.exe
