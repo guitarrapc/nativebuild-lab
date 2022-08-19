a2f185548cfc13a5e27f3aaff7197a7c59738e9b
Ikiru Yoshizaki <3856350+guitarrapc@users.noreply.github.com>
2022-08-19 11:33:55 +0900

    feat: make lib only for darwin

--- builder/zstd/zstd-darwin-arm64.sh
+++ builder/zstd/zstd-darwin-arm64.sh
@@ -14,16 +14,17 @@ OUTPUT_DIR=${OUTPUT_DIR:=pkg/zstd/${GIT_ZSTD_VERSION}/${OS}/${PLATFORM}/}
 # build
 cd zstd
 make clean
-make CFLAGS="-target arm64-apple-macos11"
+# make CFLAGS="-target arm64-apple-macos11"
+make CFLAGS="-target arm64-apple-macos11" lib # github action alrady include x86_64 LZ4/LZMA and failed on arm64.
 cd ..
 
 # confirm
 ls zstd/lib/libzstd.a
 ls zstd/lib/libzstd.*dylib
-ls zstd/programs/zstd
+# ls zstd/programs/zstd
 
 # copy
 mkdir -p "./${OUTPUT_DIR}/"
 cp ./zstd/lib/libzstd.a "./${OUTPUT_DIR}/."
 cp "./zstd/lib/libzstd.${FILE_ZSTD_VERSION}.dylib" "./${OUTPUT_DIR}/libzstd.dylib"
-cp ./zstd/programs/zstd "./${OUTPUT_DIR}/."
+# cp ./zstd/programs/zstd "./${OUTPUT_DIR}/."
