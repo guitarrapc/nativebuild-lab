de595104959b7d3d75b2fe5a7f6e2599648b7249
Ikiru Yoshizaki <3856350+guitarrapc@users.noreply.github.com>
2022-08-31 17:05:32 +0900

    fix: build

--- builder/mbedtls/mbedtls-windows-arm64.bat
+++ builder/mbedtls/mbedtls-windows-arm64.bat
@@ -18,9 +18,9 @@ dir %SRC_DIR%\cmake\build.dir\library\Release\*.dll
 
 :: copy
 mkdir %OUTPUT_DIR%
-cp %SRC_DIR%\cmake\build.dir\library\Release\%WIN_LIBNAME_CRYPTO%.dll .\%OUTPUT_DIR%\%LIBNAME_CRYPTO%.dll
-cp %SRC_DIR%\cmake\build.dir\library\Release\%WIN_LIBNAME_CRYPTO%.lib .\%OUTPUT_DIR%\%LIBNAME_CRYPTO%.lib
-cp %SRC_DIR%\cmake\build.dir\library\Release\%WIN_LIBNAME_TLS%.dll .\%OUTPUT_DIR%\%LIBNAME_TLS%.dll
-cp %SRC_DIR%\cmake\build.dir\library\Release\%WIN_LIBNAME_TLS%.lib .\%OUTPUT_DIR%\%LIBNAME_TLS%.lib
-cp %SRC_DIR%\cmake\build.dir\library\Release\%WIN_LIBNAME_X509%.dll .\%OUTPUT_DIR%\%LIBNAME_X509%.dll
-cp %SRC_DIR%\cmake\build.dir\library\Release\%WIN_LIBNAME_X509%.lib .\%OUTPUT_DIR%\%LIBNAME_X509%.lib
+cp %SRC_DIR%\cmake\build.dir\library\Release\%WIN_LIBNAME_CRYPTO%.dll .\%OUTPUT_DIR%\%WIN_LIBNAME_CRYPTO%.dll
+cp %SRC_DIR%\cmake\build.dir\library\Release\%WIN_LIBNAME_CRYPTO%.lib .\%OUTPUT_DIR%\%WIN_LIBNAME_CRYPTO%.lib
+cp %SRC_DIR%\cmake\build.dir\library\Release\%WIN_LIBNAME_TLS%.dll .\%OUTPUT_DIR%\%WIN_LIBNAME_TLS%.dll
+cp %SRC_DIR%\cmake\build.dir\library\Release\%WIN_LIBNAME_TLS%.lib .\%OUTPUT_DIR%\%WIN_LIBNAME_TLS%.lib
+cp %SRC_DIR%\cmake\build.dir\library\Release\%WIN_LIBNAME_X509%.dll .\%OUTPUT_DIR%\%WIN_LIBNAME_X509%.dll
+cp %SRC_DIR%\cmake\build.dir\library\Release\%WIN_LIBNAME_X509%.lib .\%OUTPUT_DIR%\%WIN_LIBNAME_X509%.lib
