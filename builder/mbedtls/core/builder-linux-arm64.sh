b81116780e620464c123a83be050f471135d0d12
Ikiru Yoshizaki <3856350+guitarrapc@users.noreply.github.com>
2022-09-05 17:47:14 +0900

    feat: add jinja2 on linux arm64

--- builder/mbedtls/core/builder-linux-arm64.sh
+++ builder/mbedtls/core/builder-linux-arm64.sh
@@ -4,9 +4,11 @@ set -eu
 # install
 apt-get update
 apt-get install -yq --no-install-suggests --no-install-recommends make gcc libc-dev
-apt-get install -yq --no-install-suggests --no-install-recommends python3 perl
+apt-get install -yq --no-install-suggests --no-install-recommends python3 perl python3-pip
 apt-get install -yq --no-install-suggests --no-install-recommends gnutls-bin doxygen graphviz
 apt-get install -yq --no-install-suggests --no-install-recommends gcc-arm-none-eabi libnewlib-arm-none-eabi gcc-arm-linux-gnueabi libc6-dev-armel-cross
+
+pip3 install jinja2
 
 # build
 # require '-fPIC'. cipher.o: relocation R_X86_64_PC32 against symbol `mbedtls_cipher_supported' can not be used when making a shared object; recompile with -fPIC
