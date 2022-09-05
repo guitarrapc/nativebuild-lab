a9cd2d0d8cd0abbf16b2a017d108e12bf3930fa8
Ikiru Yoshizaki <3856350+guitarrapc@users.noreply.github.com>
2022-09-05 16:42:13 +0900

    feat: add mbedtls android build

---
+++ builder/mbedtls/mbedtls-android-x64.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+set -e
+
+SCRIPT_DIR=$(cd $(dirname $0); pwd)
+source $SCRIPT_DIR/settings.sh
+OS=android
+ABI=x86_64
+PLATFORM=x64
+OUTPUT_DIR=${OUTPUT_DIR:=pkg/${SRC_DIR}/${GIT_VERSION}/${OS}/${PLATFORM}/}
+
+# build
+docker run --rm -v "$SCRIPT_DIR/core:/builder" -v "$PWD/$SRC_DIR:/src" -e "ABI=$ABI" ubuntu:22.04 /bin/bash /builder/builder-android.sh
+
+# confirm
+ls $SRC_DIR/cmake/build.dir/library/$LIBNAME_CRYPTO.a
+ls $SRC_DIR/cmake/build.dir/library/$LIBNAME_CRYPTO.so*
+ls $SRC_DIR/cmake/build.dir/library/$LIBNAME_TLS.a
+ls $SRC_DIR/cmake/build.dir/library/$LIBNAME_TLS.so*
+ls $SRC_DIR/cmake/build.dir/library/$LIBNAME_X509.a
+ls $SRC_DIR/cmake/build.dir/library/$LIBNAME_X509.so*
+
+# copy
+mkdir -p "./${OUTPUT_DIR}/"
+cp ./$SRC_DIR/cmake/build.dir/library/$LIBNAME_CRYPTO.a "./${OUTPUT_DIR}/."
+cp ./$SRC_DIR/cmake/build.dir/library/$LIBNAME_CRYPTO.so "./${OUTPUT_DIR}/."
+cp ./$SRC_DIR/cmake/build.dir/library/$LIBNAME_TLS.a "./${OUTPUT_DIR}/."
+cp ./$SRC_DIR/cmake/build.dir/library/$LIBNAME_TLS.so "./${OUTPUT_DIR}/."
+cp ./$SRC_DIR/cmake/build.dir/library/$LIBNAME_X509.a "./${OUTPUT_DIR}/."
+cp ./$SRC_DIR/cmake/build.dir/library/$LIBNAME_X509.so "./${OUTPUT_DIR}/."
