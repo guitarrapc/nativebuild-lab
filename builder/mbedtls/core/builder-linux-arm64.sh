873124b897214a5f552815f217036649d9ba075b
Ikiru Yoshizaki <3856350+guitarrapc@users.noreply.github.com>
2022-09-07 12:36:57 +0900

    fix: mbedtls Linux arm64 artifact

--- builder/mbedtls/core/builder-linux-arm64.sh
+++ builder/mbedtls/core/builder-linux-arm64.sh
@@ -6,12 +6,11 @@ apt-get update
 apt-get install -yq --no-install-suggests --no-install-recommends make gcc libc-dev
 apt-get install -yq --no-install-suggests --no-install-recommends python3 perl python3-pip
 apt-get install -yq --no-install-suggests --no-install-recommends gnutls-bin doxygen graphviz
-apt-get install -yq --no-install-suggests --no-install-recommends gcc-arm-none-eabi libnewlib-arm-none-eabi gcc-arm-linux-gnueabi libc6-dev-armel-cross
+apt-get install -yq --no-install-suggests --no-install-recommends gcc-aarch64-linux-gnu
 
 pip3 install jinja2
 
 # build
-# require '-fPIC'. cipher.o: relocation R_X86_64_PC32 against symbol `mbedtls_cipher_supported' can not be used when making a shared object; recompile with -fPIC
 cd /src
 make clean
-make SHARED=1 CFLAGS="-O2 -Werror -fPIC" lib
+make SHARED=1 CC=aarch64-linux-gnu-gcc CFLAGS="-O2 -Werror" lib
