fe8ff7d870460a1ad4bb192a63e2f24d107d230a
Ikiru Yoshizaki <3856350+guitarrapc@users.noreply.github.com>
2022-09-28 19:07:46 +0900

    feat: add dummy header entry

--- fibo/lib/dummy.h
+++ fibo/lib/dummy.h
@@ -1,6 +1,6 @@
 typedef uint64_t mbedtls_mpi_uint;
   typedef uint64_t piyopiyo;
-#  typedef uint64_t hogemoge;
+//  typedef uint64_t should_be_ignopre_typdef;
 
     typedef struct
     {
@@ -27,3 +27,20 @@ mbedtls_mpi_uint mbedtls_mpi_core_mla( mbedtls_mpi_uint *d, size_t d_len ,
 void foo();
 
 FIBOLIB_API void get_sample_data(sample_data_t *output);
+FIBOLIB_API int fibo(int n);
+// void should_be_ignopre_method();
+
+#ifndef FIBOLIB_VISIBILITY
+#  if defined(__GNUC__) && (__GNUC__ >= 4)
+#    define FIBOLIB_VISIBILITY __attribute__ ((visibility ("default")))
+#  else
+#    define FIBOLIB_VISIBILITY
+#  endif
+#endif
+#if defined(DLL_EXPORT)
+#  define FIBOLIB_API __declspec(dllexport) FIBOLIB_VISIBILITY
+#elif defined(DLL_IMPORT)
+#  define FIBOLIB_API __declspec(dllimport) FIBOLIB_VISIBILITY /* It isn't required but allows to generate better code, saving a function pointer load from the IAT and an indirect jump.*/
+#else
+#  define FIBOLIB_API FIBOLIB_VISIBILITY
+#endif
