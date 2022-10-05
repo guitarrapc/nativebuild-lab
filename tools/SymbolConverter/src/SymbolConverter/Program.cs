037bf83c47117f018f31fa70e9c7943268e4fc06
Ikiru Yoshizaki <3856350+guitarrapc@users.noreply.github.com>
2022-10-05 18:27:22 +0900

    feat: file paths

--- tools/SymbolConverter/src/SymbolConverter/Program.cs
+++ tools/SymbolConverter/src/SymbolConverter/Program.cs
@@ -9,12 +9,12 @@ public class SymbolApp : ConsoleAppBase
 {
     [Command("list", "List symbols from header files (.h)")]
     public async Task List(
-        [Option("header-path", "Directory path contains header files.")]string headerPath)
+        [Option("header-paths", "libary directory path to search header files.")] string[] headerPaths)
     {
-        var list = await SymbolOperation.ListAsync(headerPath);
+        var list = await SymbolOperation.ListAsync(headerPaths);
         var symbolLookup = list.ToLookup(x => x!.GetSourceFile());
 
-        Console.WriteLine($@"Source directory: {headerPath}");
+        Console.WriteLine($@"Source directory: {string.Join(",", headerPaths)}");
         Console.WriteLine();
         foreach (var symbols in symbolLookup)
         {
@@ -33,63 +33,72 @@ public class SymbolApp : ConsoleAppBase
 
     [Command("prefix", "Add prefix to header and implemataion files.")]
     public async Task Prefix(
-        [Option("header-path", "Directory path contains header files.")] string headerPath,
+        [Option("header-paths", "Directory path contains header files.")] string[] headerPaths,
         [Option("prefix", "Prefix to add.")] string prefix,
-        [Option("impl-path", "Directory path contains implementaion files. (Default: Use headerPath)")] string implPath = "",
+        [Option("impl-paths", "Directory path contains implementaion files. (Default: Use headerPath)")] string[]? implPaths = null,
         [Option("dryrun", "True to dry-run. false to apply changes.")] bool dryrun = true
     )
     {
-        if (string.IsNullOrEmpty(implPath))
+        if (implPaths is null)
         {
-            implPath = headerPath;
+            implPaths = headerPaths;
         }
 
-        var symbols = await SymbolOperation.ListAsync(headerPath, prefix);
+        var symbols = await SymbolOperation.ListAsync(headerPaths, prefix);
 
         // header
         Console.WriteLine();
-        Console.WriteLine($@"Header Source directory: {headerPath}");
-        var headerFiles = Directory.EnumerateFiles(headerPath, "*.h", new EnumerationOptions { RecurseSubdirectories = true });
-        await SymbolOperation.ReplaceAsync(headerFiles, symbols, dryrun);
+        Console.WriteLine($@"Header Source directory: {string.Join(",", headerPaths)}");
+        foreach (var headerPath in headerPaths)
+        {
+            var headerFiles = Directory.EnumerateFiles(headerPath, "*.h", new EnumerationOptions { RecurseSubdirectories = true });
+            await SymbolOperation.ReplaceAsync(headerFiles, symbols, dryrun);
+        }
 
         // impl
         Console.WriteLine();
-        Console.WriteLine($@"Impl Source directory: {implPath}");
-        var implFiles = Directory.EnumerateFiles(implPath, "*.c", new EnumerationOptions { RecurseSubdirectories = true });
-        await SymbolOperation.ReplaceAsync(implFiles, symbols, dryrun);
+        Console.WriteLine($@"Impl Source directory: {string.Join(",", implPaths)}");
+        foreach (var implPath in implPaths)
+        {
+            var implFiles = Directory.EnumerateFiles(implPath, "*.c", new EnumerationOptions { RecurseSubdirectories = true });
+            await SymbolOperation.ReplaceAsync(implFiles, symbols, dryrun);
+        }
     }
 }
 
 public static class SymbolOperation
 {
-    public static async Task<IReadOnlyList<SymbolInfo?>> ListAsync(string headerPath, string prefix = "")
+    public static async Task<IReadOnlyList<SymbolInfo?>> ListAsync(string[] headerPaths, string prefix = "")
     {
-        var headerFiles = Directory.EnumerateFiles(headerPath, "*.h", new EnumerationOptions { RecurseSubdirectories = true });
-        var reader = new SymbolReader();
-
         var list = new List<SymbolInfo?>();
-        foreach (var file in headerFiles)
+        var reader = new SymbolReader();
+        foreach (var headerPath in headerPaths)
         {
-            Debug.WriteLine($"Reading file '{file}'");
+            var headerFiles = Directory.EnumerateFiles(headerPath, "*.h", new EnumerationOptions { RecurseSubdirectories = true });
 
-            var content = await File.ReadAllLinesAsync(file);
-            var externFields = reader.Read(DetectionType.ExternField, content, s => prefix + s, file);
-            var methods = reader.Read(DetectionType.Method, content, s => prefix + s, file);
-            var typedefs = reader.Read(DetectionType.Typedef, content, s => prefix + s, file);
-
-            if (externFields.Any())
+            foreach (var file in headerFiles)
             {
-                list.AddRange(externFields);
-            }
+                Debug.WriteLine($"Reading file '{file}'");
 
-            if (methods.Any())
-            {
-                list.AddRange(methods);
-            }
+                var content = await File.ReadAllLinesAsync(file);
+                var externFields = reader.Read(DetectionType.ExternField, content, s => prefix + s, file);
+                var methods = reader.Read(DetectionType.Method, content, s => prefix + s, file);
+                var typedefs = reader.Read(DetectionType.Typedef, content, s => prefix + s, file);
 
-            if (typedefs.Any())
-            {
-                list.AddRange(typedefs);
+                if (externFields.Any())
+                {
+                    list.AddRange(externFields);
+                }
+
+                if (methods.Any())
+                {
+                    list.AddRange(methods);
+                }
+
+                if (typedefs.Any())
+                {
+                    list.AddRange(typedefs);
+                }
             }
         }
         return list;
