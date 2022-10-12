8ccee01873d72225b52223d9ac2c559288d1b6af
Ikiru Yoshizaki <3856350+guitarrapc@users.noreply.github.com>
2022-10-13 03:14:38 +0900

    feat: 1225

--- tools/SymbolConverter/src/SymbolConverter/Program.cs
+++ tools/SymbolConverter/src/SymbolConverter/Program.cs
@@ -57,7 +57,7 @@ public class SymbolApp : ConsoleAppBase
         if (list.Any())
         {
             // remove duplicate symbols
-            var symbols = list.DistinctBy(x => x!.Symbol).Take(1200).ToArray();
+            var symbols = list.DistinctBy(x => x!.Symbol).Take(1225).ToArray();
             Console.WriteLine("Replace following symbols...");
             foreach (var symbol in symbols)
             {
