d753cc133291ac9f4f9b6e6db12a06bf16159669
Ikiru Yoshizaki <3856350+guitarrapc@users.noreply.github.com>
2022-10-13 04:21:57 +0900

    feat: remove take

--- tools/SymbolConverter/src/SymbolConverter/Program.cs
+++ tools/SymbolConverter/src/SymbolConverter/Program.cs
@@ -58,7 +58,7 @@ public class SymbolApp : ConsoleAppBase
         if (list.Any())
         {
             // remove duplicate symbols
-            var symbols = list.DistinctBy(x => x!.Symbol).Take(1500).ToArray();
+            var symbols = list.DistinctBy(x => x!.Symbol).ToArray();
             Console.WriteLine("Replace following symbols...");
             foreach (var symbol in symbols)
             {
