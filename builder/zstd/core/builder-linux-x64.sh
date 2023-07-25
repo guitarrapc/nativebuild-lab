75421d07781382f1695bc91cdde606a1fdb07fe9
Ikiru Yoshizaki <3856350+guitarrapc@users.noreply.github.com>
2023-07-25 18:26:16 +0900

    feat: add zstd cmake amd64 build

---
+++ builder/zstd/core/builder-linux-x64.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+set -eu
+
+apt update
+apt install -yq --no-install-suggests --no-install-recommends make gcc libc-dev cmake file
+apt install -yq --no-install-suggests --no-install-recommends build-essential # fix `No CMAKE_CXX_COMPILER could be found.`
+
+SRC_DIR="/src"
+BUILD_DIR=$SRC_DIR/build/cmake/build
+rm -rf $BUILD_DIR
+mkdir -p $BUILD_DIR
+
+CMAKE_TOOLCHAIN=$BUILD_DIR/toolchain.cmake
+cat <<EOF | tee "${CMAKE_TOOLCHAIN}"
+  set(CMAKE_SYSTEM_NAME "Linux")
+EOF
+
+pushd $BUILD_DIR
+  cmake -DCMAKE_BUILD_TYPE=Release ..
+  cmake --build . --config Release
+popd
