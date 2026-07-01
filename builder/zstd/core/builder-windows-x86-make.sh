fa70183e4ba425b4ab662d0763e5be1ca3c55fb0
Ikiru Yoshizaki <3856350+guitarrapc@users.noreply.github.com>
2026-07-01 19:47:48 +0900

    ci: fix x86 Windows build

--- builder/zstd/core/builder-windows-x86-make.sh
+++ builder/zstd/core/builder-windows-x86-make.sh
@@ -6,4 +6,5 @@ apt-get update
 apt-get install -yq --no-install-suggests --no-install-recommends make gcc libc-dev sudo mingw-w64
 cd /src
 make clean
-make EXT=.exe TARGET_SYSTEM=Windows CC=i686-w64-mingw32-gcc WINDRES=i686-w64-mingw32-windres AR=i686-w64-mingw32-ar
+make lib EXT=.exe TARGET_SYSTEM=Windows CC=i686-w64-mingw32-gcc WINDRES=i686-w64-mingw32-windres AR=i686-w64-mingw32-ar
+make -C programs zstd-nomt EXT=.exe TARGET_SYSTEM=Windows CC=i686-w64-mingw32-gcc WINDRES=i686-w64-mingw32-windres AR=i686-w64-mingw32-ar
